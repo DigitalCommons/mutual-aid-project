67e8febaaf221f75ea560c236ddb636410b9668f
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-07-24 19:40:13 +0100

    src/config.ts - update for mm 3.0.7

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,11 @@
-// Re-export of ConfigData in sea-map/index above seems not to work,
-// so import it directly from here:
-import type { ConfigData } from  "sea-map/app/model/config-schema";
-import type {
-  Initiative, SseInitiative, CustomPropDef, InitiativeObj
-} from "mykomap/app/model/data-services";
-import { getAddress, getEmail, getTwitter } from "mykomap/app/view/map/default-popup";
-import about from "../config/about.html";
-import * as versions from "../config/version.json";
+import { ConfigData } from  "mykomap/app/model/config-schema";
+import type { DataServices } from "mykomap/app/model/data-services";
+import { getAddress, getEmail, getTwitter } from "mykomap/app/default-popup";
+import about from "./about.html";
+import * as versions from "./version.json";
+import { Initiative } from "mykomap/src/map-app/app/model/initiative";
 
-function customPopup(initiative: Initiative, model: SseInitiative): string {
+function customPopup(initiative: Initiative, model: DataServices): string {
   const labels = model.getFunctionalLabels();
   let popupHTML = `
     <div class="sea-initiative-details">
@@ -31,7 +28,7 @@ function customPopup(initiative: Initiative, model: SseInitiative): string {
 }
 
 
-const config: ConfigData = {
+export const config = new ConfigData({
   customPopup: customPopup,
   namedDatasets : ["mutual-aid"],
   namedDatasetsVerbose : ["Mutual-Aid Organisations"],
@@ -41,24 +38,29 @@ const config: ConfigData = {
     twitter: 'value',
     alphabetical: {
       type: 'custom',
-      calling: (id, def, params) => {
-        return params.name.charAt(0);
+      builder: (id, def, params) => {
+        var name = params.name;
+        return typeof name === 'string'? name.charAt(0) : '';
       },
     },
   },
   languages: ["EN"],
-  vocabularies: [
-    { "endpoint": "http:\/\/dev.data.solidarityeconomy.coop:8890/sparql",
-      "uris": {}
-    }
+  vocabularies: [],
+  dataSources: [
+    {
+      id: 'mutual-aid',
+      label: 'Multia Aid',
+      type: 'hostSparql',
+    },
   ],
   showDatasetsPanel : false,
   showDirectoryPanel : false,
   htmlTitle : "Mutual Aid",
   tileUrl : "https:/\/tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=bb1cf9a95944474da77e513385d19680",
   mapAttribution :  "Maps &copy; <a href='http:/\/www.thunderforest.com'>Thunderforest</a> & &copy; <a href='http:/\/www.openstreetmap.org/copyright'>OpenStreetMap contributors</a> | Other data <a href='http:/\/creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a> | Powered by <a href='https:/\/www.geoapify.com/'>Geoapify</a>",
-  defaultLatLng: [50.84999, 4.39434]
-};
+  aboutHtml: about,
+  defaultLatLng: [50.84999, 4.39434],
+  ...versions
+});
 
-export const combinedConfig = { ...config, ...versions, aboutHtml: about } as ConfigData;
 
